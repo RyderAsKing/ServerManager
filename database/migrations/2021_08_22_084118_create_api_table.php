1b7d795a651ec3cd755c0c0a2e6614020ac19691
RyderAsking <clasherlodt@gmail.com>
2021-08-22 14:17:00 +0530

    Migration for API table

---
+++ database/migrations/2021_08_22_084118_create_api_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('type');
+            $table->string('api', 17);
+            $table->string('api_pass', 33);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('api');
+    }
+}
