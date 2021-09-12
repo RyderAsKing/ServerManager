635a0514f6613c1493af5ad2f3dacd7fbe53408e
RyderAsking <clasherlodt@gmail.com>
2021-09-12 10:33:55 +0530

    Adder better response messages

--- app/Http/Controllers/Api/ServerController.php
+++ app/Http/Controllers/Api/ServerController.php
@@ -50,7 +50,7 @@ class ServerController extends Controller
     public function power(Request $request, $id)
     {
         if (empty($request->bearerToken())) {
-            return response()->json(['message' => 'Unauthorized']);
+            return response()->json(['status' => 401, 'message' => 'Unauthorized']);
         }
         $user = ApiFunctions::returnUser($request->bearerToken());
         $server = $user->server()->where(['id' => $id])->firstOrFail();
@@ -60,7 +60,7 @@ class ServerController extends Controller
         $response = array();
 
         if ($action != 'start' && $action != 'stop' && $action != 'restart' &&  $action != 'kill') {
-            return response()->json(["message" => "Invalid method"]);
+            return response()->json(['satus' => 404, "message" => "Invalid method"]);
         }
 
         if ($type == 1) {
@@ -68,20 +68,20 @@ class ServerController extends Controller
             $action = json_encode($action);
             $response = PterodactylFunctions::sendPowerAction($server, $api_instance, $action);
             if (empty($response)) {
-                $response = array('message' => 'Action successful');
+                $response = array('status' => 200, 'message' => 'Power action executed successfully');
             }
         }
         if ($type == 0) {
             $v = VirtualizorFunctions::createVirtualizorClient($api_instance);
             $information = VirtualizorFunctions::getVirtualizorInformation($v, $server);
             if (empty($information)) {
-                return response()->json(['message' => 'The host returned a empty response, check your API, API Pass and try again. If it still does not work then there is high possibility that your host has a invalid license or has blocked the API requests.']);
+                return response()->json(['status' => 419, 'message' => 'The host returned a empty response, check your API, API Pass and try again. If it still does not work then there is high possibility that your host has a invalid license or has blocked the API requests.']);
             }
             $response = VirtualizorFunctions::sendPowerAction($v, $server, $action);
             if (empty($response)) {
-                return response()->json(["message" => "Invalid response from host"]);
+                return response()->json(['status' => 419, "message" => "Invalid response from host"]);
             }
-            $response = array('message' => $response);
+            $response = array('status' => 200, 'message' => $response);
         }
         return response()->json($response);
     }
