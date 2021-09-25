5c0c4092f79a06f41fd0d366619b5fe9ef9c2595
RyderAsking <clasherlodt@gmail.com>
2021-09-25 14:44:51 +0530

    Removed unnecessary code

--- resources/views/dashboard/api/modify.blade.php
+++
@@ -1,8 +0,0 @@
-@extends("layout.app")
-@section("content")
-<div class="container text-white">
-    <h3>Add a API</h3>
-    <form method="post" action="{{ route("dashboard.api.add") }}">
-    </form>
-</div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Server\ServerViewController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Server\ServerActionController;
-use App\Http\Controllers\Api\ApiManagementController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,39 +17,3 @@ use App\Http\Controllers\Auth\RegisterController;
 Route::get('{any}', function () {
     return view('app');
 })->where('any', '.*');
-
-/*
-Route::get('/', function () {
-    return view('home');
-})->name("home");
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->name("dashboard");
-
-Route::get('/login', [LoginController::class, 'index'])->name("login");
-Route::post('/login', [LoginController::class, 'login']);
-
-Route::get('/register', [RegisterController::class, 'index'])->name("register");
-Route::post('/register', [RegisterController::class, 'register']);
-
-Route::get('/logout', [LogoutController::class, 'index'])->name("logout");
-
-Route::get('/dashboard/server', [ServerViewController::class, 'show'])->name("dashboard.server.index");
-Route::get('/dashboard/server/add', [ServerViewController::class, 'add'])->name("dashboard.server.add"); //view
-Route::post('/dashboard/server/add', [ServerViewController::class, 'store']); //request
-
-
-Route::get('/dashboard/server/{server}', [ServerActionController::class, 'index'])->name("dashboard.server.current.index");
-Route::get('/dashboard/server/{server}/start', [ServerActionController::class, 'start'])->name("dashboard.server.current.start");
-Route::get('/dashboard/server/{server}/stop', [ServerActionController::class, 'stop'])->name("dashboard.server.current.stop");
-Route::get('/dashboard/server/{server}/restart', [ServerActionController::class, 'restart'])->name("dashboard.server.current.restart");
-Route::get('/dashboard/server/{server}/kill', [ServerActionController::class, 'kill'])->name("dashboard.server.current.kill");
-Route::get('/dashboard/server/{server}/destroy', [ServerActionController::class, 'destroy'])->name("dashboard.server.current.destroy");
-Route::post('/dashboard/server/{server}/change/hostname', [ServerActionController::class, 'changeHostname'])->name("dashboard.server.current.changehostname");
-Route::post('/dashboard/server/{server}/change/password', [ServerActionController::class, 'changePassword'])->name("dashboard.server.current.changepassword");
-
-
-Route::get('/dashboard/api', [ApiManagementController::class, 'index'])->name("dashboard.api.index"); //list
-Route::get('/dashboard/api/add', [ApiManagementController::class, 'add'])->name("dashboard.api.add"); //show a form
-Route::post('/dashboard/api/add', [ApiManagementController::class, 'store']); //request (when form filled)
-Route::get('/dashboard/api/{api}/destroy', [ApiManagementController::class, 'destroy'])->name("dashboard.api.destroy"); //particular api
-*/
